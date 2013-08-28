1f14f114782b52d6f1b2c9653d6d83ab3aa13c36
Ian Storm Taylor <ian@ianstormtaylor.com>
2013-08-28 01:21:27 -0700

    first commit

---
+++ Readme.md
@@ -0,0 +1,17 @@
+
+# on-error
+
+  Safely attach a handler to `window.onerror` regardless of existing handlers.
+
+## Installation
+
+    $ component install segmentio/on-error
+
+## API
+
+### onError(fn)
+  Call `fn` when an error is thrown on the page.
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,40 @@
+
+/**
+ * Expose `onError`.
+ */
+
+module.exports = onError;
+
+
+/**
+ * Callbacks.
+ */
+
+var callbacks = [];
+
+
+/**
+ * Preserve existing handler.
+ */
+
+if ('function' == typeof window.onerror) callbacks.push(window.onerror);
+
+
+/**
+ * Bind to `window.onerror`.
+ */
+
+window.onerror = function () {
+  for (var i = 0, fn; fn = callbacks[i]; i++) fn.apply(this, arguments);
+};
+
+
+/**
+ * Call a `fn` on `window.onerror`.
+ *
+ * @param {Function} fn
+ */
+
+function onError (fn) {
+  callbacks.push(fn);
+}
